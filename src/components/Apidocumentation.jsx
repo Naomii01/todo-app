6296156c2f4e7a9931ed5a57d22bc5ba70fe1ab4
Naomii01 <naomi222001220@gmail.com>
2024-05-08 03:53:04 +0200

    frontend api list

---
+++ src/components/Apidocumentation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ApiDocumentation = () => {
+    return (
+        <iframe
+            src="http://localhost:5000/api-doc"
+            title="API Documentation"
+            width="100%"
+            height="600px"
+            frameBorder="0"
+        />
+    );
+};
+
+export default ApiDocumentation;
